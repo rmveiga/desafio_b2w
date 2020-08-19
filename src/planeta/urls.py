5d3f4e63030998ce1732795a6cde86c5e26c9e5f
Rafael Morán <rm.veiga26@gmail.com>
2020-08-19 14:43:08 -0300

    src/planeta/api/viewsets.py - Implementada função que retorna uma lista com os nomes dos filmes em que o planeta aparece - Implementada função list do viewset para incluir na listagem do planeta a relação de filmes
    src/planeta/urls.py
    - Aplicado basename para o endpoint planetas

--- src/planeta/urls.py
+++ src/planeta/urls.py
@@ -3,4 +3,4 @@ from rest_framework import routers
 from planeta.api.viewsets import PlanetaViewset
 
 planeta_router = routers.DefaultRouter()
-planeta_router.register('planetas', PlanetaViewset)
+planeta_router.register('planetas', PlanetaViewset, basename='api-planetas')
